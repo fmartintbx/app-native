ddaca1490c0e9c0493828b67ca90baaf4f0445b5
fmartintbx <facundo.corvalan@mi.unc.edu.ar>
2024-07-11 02:23:46 -0300

    styles

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titulo}>Hello</Text>
+      <Text style={styles.subTitle}>Sign In to your account</Text>
+      <TextInput 
+        placeholder='Jhon@email.com'
+        style ={styles.textInput}
+
+      /> 
+      <TextInput 
+      placeholder='password'
+      style={styles.textInput}
+
+
+      /> 
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +31,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo:{ 
+    fontSize: 80,
+    color: '#000',
+    fontWeight: 'bold'
+  },
+  subTitle: { 
+  fontSize: 20,
+  color:'gray',
+  },
+  textInput:{ 
+  borderWidth:1,
+  borderColor: 'gray',
+  padding: 10,
+  width:'80%',
+  height:50,
+  marginTop: 20,
+  borderRadius: 30,
+
+  }
+   
+
 });
